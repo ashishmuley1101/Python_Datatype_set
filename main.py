e177b65ce8a4d2f40e0d748d7fe144e5eca65de1
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-05 17:23:38 +0530

    Python set any() method

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 
-# Python set min() method.
+# Python set any() method.
 
-# The min() function returns the smallest item in an iterable. It can also be used to
-# find the smallest item between two or more parameters.
+# The any() function returns True if any element of an iterable is True. If not, it returns False.
+# The syntax of any() is: any(iterable)
 
 
-number = {3, 2, 8, 5, 10, 6}
-smallest = min(number);
 
-print("The smallest number is:", smallest)
+# False since both are False
+l = {0, False}
+print(any(l))
 
-# Output :
-# The smallest number is: 2
+# 0 is False
+d = {0: 'False'}
+print(any(d))
+
+# 1 is True
+d = {0: 'False', 1: 'True'}
+print(any(d))
+
+# 0 and False are false
+d = {0: 'False', False: 0}
+print(any(d))
+
+# iterable is empty
+d = {}
+print(any(d))
+
+# 0 is False
+# '0' is True
+d = {'0': 'False'}
+print(any(d))
