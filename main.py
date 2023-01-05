5756cbe21892bfeefc80487f7697ec89e287594b
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-05 20:16:31 +0530

    Iterating set in python using for loop.

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 
-# Python sorted() method.
+# Iterate Over a Set in Python
 
-# The sorted() function sorts the elements of a given iterable in a specific order
-# (ascending or descending) and returns it as a list
-# The syntax of sorted() is: sorted(iterable, key=None, reverse=False)
-# sorted() can take a maximum of three parameters:
-# iterable - A sequence (string, tuple, list) or collection
-# (set, dictionary, frozen set) or any other iterator.
-# reverse (Optional) - If True, the sorted list is reversed (or sorted in descending order).
-# Defaults to False if not provided.
-# key (Optional) - A function that serves as a key for the sort comparison. Defaults to None.
+# iterating set in python using for loop.
 
-numbers = {4, 2, 12, 8}
-
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
-# Output: [2, 4, 8, 12]
-
-# vowels tuple
+# vowels
 vowels = {'e', 'a', 'u', 'o', 'i'}
-print(sorted(vowels))   # O/p : ['a', 'e', 'i', 'o', 'u']
 
-# -------Sort in descending order----------------------
-
-numbers = {4, 2, 12, 8}
-
-sorted_numbers = sorted(numbers, reverse = True)
-print(sorted_numbers)
-
-# vowels tuple
-vowels = {'e', 'a', 'u', 'o', 'i'}
-print(sorted(vowels,reverse = True))
+for vow in vowels:
+    print(vow)
 
 # Output :
-# [12, 8, 4, 2]
-# ['u', 'o', 'i', 'e', 'a']
+# i
+# e
+# o
+# a
+# u
 
