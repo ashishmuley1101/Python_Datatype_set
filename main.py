90c77f7c499bcbd44f7087c6e9a51c7197a57156
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-05 20:08:32 +0530

    Python sorted() method.

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 
-# Python set sum() method.
+# Python sorted() method.
 
-# The sum() function adds the items of an iterable and returns the sum.
-# The syntax of sum() is: sum(iterable, start)
-# The sum() function adds start and items of the given iterable from left to right.
-# iterable - iterable (list, tuple, dict, etc). The items of the iterable should be *numbers*.
-# start (optional) - this value is added to the sum of items of the iterable. The default value
-# of start is 0 (if omitted).
+# The sorted() function sorts the elements of a given iterable in a specific order
+# (ascending or descending) and returns it as a list
+# The syntax of sorted() is: sorted(iterable, key=None, reverse=False)
+# sorted() can take a maximum of three parameters:
+# iterable - A sequence (string, tuple, list) or collection
+# (set, dictionary, frozen set) or any other iterator.
+# reverse (Optional) - If True, the sorted list is reversed (or sorted in descending order).
+# Defaults to False if not provided.
+# key (Optional) - A function that serves as a key for the sort comparison. Defaults to None.
 
-numbers = [2.5, 3, 4, -5]
+numbers = {4, 2, 12, 8}
 
-# start parameter is not provided
-numbers_sum = sum(numbers)
-print(numbers_sum)
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+# Output: [2, 4, 8, 12]
 
-# start = 10
-numbers_sum = sum(numbers, 10)
-print(numbers_sum)
+# vowels tuple
+vowels = {'e', 'a', 'u', 'o', 'i'}
+print(sorted(vowels))   # O/p : ['a', 'e', 'i', 'o', 'u']
 
-# Output
-# 4.5
-# 14.5
+# -------Sort in descending order----------------------
+
+numbers = {4, 2, 12, 8}
+
+sorted_numbers = sorted(numbers, reverse = True)
+print(sorted_numbers)
+
+# vowels tuple
+vowels = {'e', 'a', 'u', 'o', 'i'}
+print(sorted(vowels,reverse = True))
+
+# Output :
+# [12, 8, 4, 2]
+# ['u', 'o', 'i', 'e', 'a']
+
