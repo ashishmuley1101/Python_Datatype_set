73d4c7556ef16069b82887a46df73b46d892f090
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-05 19:46:36 +0530

    Python set sum() method.

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 
-# Python set enumerate() method.
+# Python set sum() method.
 
-# The enumerate() method adds a counter to an iterable and returns it (the enumerate object).
-# You can convert enumerate objects to list and tuple using list() and tuple() method respectively
-# The syntax of enumerate() is: enumerate(iterable, start=0)
-# iterable - a sequence, an iterator, or objects that supports iteration
-# start (optional) - enumerate() starts counting from this number.
-# If start is omitted, 0 is taken as start.
+# The sum() function adds the items of an iterable and returns the sum.
+# The syntax of sum() is: sum(iterable, start)
+# The sum() function adds start and items of the given iterable from left to right.
+# iterable - iterable (list, tuple, dict, etc). The items of the iterable should be *numbers*.
+# start (optional) - this value is added to the sum of items of the iterable. The default value
+# of start is 0 (if omitted).
 
-languages = {'Python', 'Java', 'JavaScript'}
+numbers = [2.5, 3, 4, -5]
 
-enumerate_lang = enumerate(languages)
+# start parameter is not provided
+numbers_sum = sum(numbers)
+print(numbers_sum)
 
-# convert enumerate object to list
-print(list(enumerate_lang))
-# Output: [(0, 'Python'), (1, 'Java'), (2, 'JavaScript')]
+# start = 10
+numbers_sum = sum(numbers, 10)
+print(numbers_sum)
 
-grocery = {'bread', 'milk', 'butter'}
-enumerateGrocery = enumerate(grocery)
-
-print(type(enumerateGrocery))
-
-# converting to list
-print(list(enumerateGrocery))
-
-# changing the default counter
-enumerateGrocery = enumerate(grocery, 10)  # start with 10
-print(list(enumerateGrocery))
 # Output
-# <class 'enumerate'>
-# [(0, 'bread'), (1, 'milk'), (2, 'butter')]
-# [(10, 'bread'), (11, 'milk'), (12, 'butter')]
+# 4.5
+# 14.5
